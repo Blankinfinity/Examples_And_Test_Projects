27bafd6d3fd901f0be23bfb8e77d825709e52844
Keith Pearce <keith@keith-pearce.co.uk>
2020-06-12 22:45:23 +0100

    Adding XOR Encryption project.

---
+++ Example_And_Test_Projects/XOREncryption/Xor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace XOREncryption
+{
+    public static class Xor
+    {
+        public static string EncryptDecrypt(string text, int key)
+        {
+            var newText = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var charValue = Convert.ToInt32(text[i]); //get the ASCII value of the character
+                charValue ^= key; //xor the value
+
+                newText += char.ConvertFromUtf32(charValue); //convert back to string
+            }
+
+            return newText;
+        }
+    }
+}
